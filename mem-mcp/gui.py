11e7815a6a6072f48ec95c764884f538c9c6cb45
Gergely Papp <gergely.papp@itworks.hu>
2026-04-29 11:51:33 +0200

    Fixing mcp reg command

--- mem-mcp/gui.py
+++ mem-mcp/gui.py
@@ -221,7 +221,7 @@ def _get_auth_context(request: Request):
     
     # Intelligently calculate MCP_URL
     # If BASE_URL already ends in /mcp, don't append it again
-    mcp_url = f"{mem.BASE_URL}/mcp"
+    mcp_url = f"{mem.BASE_URL}/mcp/"
     if mem.BASE_URL.endswith("/mcp"):
         mcp_url = mem.BASE_URL
         
